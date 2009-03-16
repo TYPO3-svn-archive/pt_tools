7cbd233645ad5a9ede8cebcc6e4aed8673469302
sonic <sonic@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-03-16 10:41:43 +0000

    ADD: added a smarty modifier for date format conversion
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/pt_tools/trunk@17860 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ res/smarty_plugins/modifier.convertDate.php
@@ -0,0 +1,45 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*  
+*  (c) 2009 Rainer Kuhn <kuhn@punkt.de>
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is 
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+* 
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+* 
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+	
+	
+require_once t3lib_extMgm::extPath('pt_tools').'res/staticlib/class.tx_pttools_div.php';
+
+/**
+ * Smarty modifier for date format conversion: converts a given date string from Euro format to US format or vice versa and returns the converted string
+ * Usage: {"datestring"|convertDate}   if you want to convert from Euro format (TT.MM.YYYY) to US format (YYYY-MM-DD)
+ * Usage: {"datestring"|convertDate:1} if you want to convert from US format (YYYY-MM-DD) to Euro format (TT.MM.YYYY)
+ *
+ * @param   string      original date string to convert
+ * @param   boolean     (optional) flag for conversion direction: 0 (default) = converts DD.MM.YYYY/DD-MM-YYYY to YYYY-MM-DD, 1 = converts YYYY-MM-DD to DD.MM.YYYY
+ * @return  string      converted date (YYYY-MM-DD by default, if 2. param is set to true DD.MM.YYYY)
+ * @author	Rainer Kuhn <kuhn@punkt.de>
+ * @since	2009-03-16
+ */
+function smarty_modifier_convertDate($dateOrig, $reverse=0) {
+	
+	return tx_pttools_div::convertDate($dateOrig, $reverse);
+	
+}
+
+?>
