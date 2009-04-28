acee205647f4ca912bbb0bb6467b1a6fae83ab3d
sonic <sonic@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-04-28 11:45:29 +0000

    ADD: added a smarty modifier for tracing/debugging output: res/smarty_plugins/modifier.trace.php
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/pt_tools/trunk@19602 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ res/smarty_plugins/modifier.trace.php
@@ -0,0 +1,52 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*  
+*  (c) 2009 Rainer Kuhn <kuhn@punkt.de>
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is 
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+* 
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+* 
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+	
+	
+require_once t3lib_extMgm::extPath('pt_tools').'res/staticlib/class.tx_pttools_div.php';
+
+/**
+ * Smarty modifier for tracing/debugging output: displays a given variable value with it's name if the $traceFlag is set to true
+ * Usage: {$myVar|trace:'myVarName':$trace}
+ *        To assign $trace in the smarty template:  {assign var="trace" value=0} respective {assign var="trace" value=1}
+ *
+ * @param   mixed       abribtrary value to display
+ * @param   string      name of the value to display
+ * @param   boolean     flag wether tracing/debugging debugging output is enabled - use a smarty variable $trace passed to your template for this, 
+ *                      or assign an appropriate smarty variable in the template itself, e.g. {assign var="trace" value=1}
+ * @return  string      converted date (YYYY-MM-DD by default, if 2. param is set to true DD.MM.YYYY)
+ * @throws  tx_pttools_exceptionAssertion   if non-empty date param is not a string
+ * @author	Rainer Kuhn <kuhn@punkt.de>
+ * @since	2009-04-28
+ */
+function smarty_modifier_trace($value, $name, $traceFlag) {
+    
+    if (!$traceFlag) {
+        return;
+    }
+    
+    return '<code>['.$name.': '.$value.']</code>';
+	
+}
+
+?>
